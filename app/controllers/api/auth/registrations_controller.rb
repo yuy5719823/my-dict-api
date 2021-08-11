20ebada8c76a0c600abbd1144b159d7d1587910d
yuy5719823 <youya_827@yahoo.co.jp>
2021-08-11 22:45:28 +0900

    [Add]devise-token-authの追加、設定

---
+++ app/controllers/api/auth/registrations_controller.rb
@@ -0,0 +1,9 @@
+class Api::Auth::RegistrationsController < DeviseTokenAuth::RegistrationsController
+
+  private
+
+    def sign_up_params
+      params.require(:registration).permit(:name, :email, :password, :password_confirmation)
+    end
+
+end
