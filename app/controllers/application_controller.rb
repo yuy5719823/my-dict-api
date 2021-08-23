4445e3cc293ae83a4769efea9aacfb434d066adf
yuy5719823 <youya_827@yahoo.co.jp>
2021-08-23 12:31:51 +0900

    [Edit]アカウント情報の更新時パラメータ

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,11 @@
 class ApplicationController < ActionController::API
         include DeviseTokenAuth::Concerns::SetUserByToken
         # skip_before_action :verify_authenticity_token, if: :devise_controller?
+        before_action :configure_permitted_parameters, if: :devise_controller?
+
 
         def configure_permitted_parameters
                 devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+                devise_parameter_sanitizer.permit(:account_update, keys: [:name])
         end
 end
